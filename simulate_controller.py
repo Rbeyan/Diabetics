2d6a5479f635c9a63821d1786c5e724d63164aa9
Rbeyan <78441758+Rbeyan@users.noreply.github.com>
2023-12-14 18:46:19 +0100

    Add files via upload

---
+++ simulate_controller.py
@@ -0,0 +1,47 @@
+"""
+Changes that have been made:
+- remove unused imports and variables
+"""
+
+import glob
+import os
+from datetime import datetime, timedelta
+from warnings import simplefilter
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+simplefilter(action='ignore', category=FutureWarning)
+simplefilter(action='ignore', category=UserWarning)
+
+from simglucose.actuator.pump import InsulinPump
+from simglucose.controller.pid_ctrller import PIDController
+from simglucose.patient.t1dpatient import T1DPatient
+from simglucose.sensor.cgm import CGMSensor
+from simglucose.simulation.env import T1DSimEnv
+from simglucose.simulation.scenario import CustomScenario
+from simglucose.simulation.sim_engine import SimObj, sim
+from controller import PPOController
+from simglucose_gym_env import T1DAdultSimEnv
+from custom_rewards import partial_negativity
+
+latest_saved_model = r"C:\Users\yanni\diabetes-RL-project\test\new_model\adult.zip"
+
+controller = PPOController(0, latest_saved_model)
+
+now = datetime.now()
+start_time = datetime.combine(now.date(), datetime.min.time())
+path = r"C:\Users\yanni\diabetes-RL-project\results"
+
+patient = T1DPatient.withName('adult#005')
+sensor = CGMSensor.withName('Dexcom', seed=1)
+pump = InsulinPump.withName('Insulet')
+
+scen = [(2, 70), (10, 30), (14, 110), (21, 90)]
+scenario = CustomScenario(start_time=start_time, scenario=scen)
+env = T1DSimEnv(patient, sensor, pump, scenario)
+
+s1 = SimObj(env, controller, timedelta(days=1), animate=True, path=path)
+sim(s1)
+
+#pid_controller = PIDController(P=-0.0001, I=-0.000000275, D=-0.1)
+#s2 = SimObj(env, pid_controller, timedelta(days=1), animate=True, path=path)
+#sim(s2)
